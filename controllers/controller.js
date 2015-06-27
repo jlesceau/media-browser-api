f2a790782cb8ca13708d3981c82bc17f93dc9fb1
Jérémy Lesceau <jeremy.lesceau@gmail.com>
2015-06-27 18:13:41 +0200

    Refactored routes and error handling

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,62 +1,140 @@
-var tree = require('../data/tree.js'),
+var fs = require('fs'),
     diskspace = require('diskspace'),
+    tree = require('../data/tree.js'),
     controller = {};
 
-controller.getDisk = function(callback) {
+function fail(res, err, message) {
+  res.statusCode = 500;
+  res.send({
+    result: null,
+    message: ( message || '' ),
+    error: err
+  });
+}
+
+function success(res, result, message) {
+  res.statusCode = 200;
+  res.send({
+    result: result,
+    message: ( message || '' ),
+    error: null
+  });
+}
+
+controller.getDisk = function(req, res) {
   diskspace.check(
-      tree.get('topDirectory'),
-      function (err, total, free, status) {
-        callback({
+    tree.get('topDirectory'),
+    function (err, total, free, status) {
+      if (err)
+        fail(res, err);
+      else
+        success(res, {
           total: total,
           free: free
         });
-      }
+    }
   );
 };
 
-controller.getSeries = function() {
-  return tree.get('series');
+// Series
+controller.getSeries = function(req, res) {
+  success(res, tree.get('series'));
 };
+controller.getSerie = function(req, res) {
+  var serie;
 
-controller.getSerie = function(serie_id) {
-  return tree.get('series', { title: serie_id });
+  if ((serie = tree.get('series', { title: req.params.serie_id })))
+    success(res, serie);
+  else
+    fail(res, null, 'Series not found');
 };
+controller.getSeason = function(req, res) {
+  var season;
 
-controller.getSeason = function(serie_id, season_id) {
-  return tree.get('series',
-      { title: serie_id },
+  if ((
+    season = tree.get(
+      'series',
+      { title: req.params.serie_id },
       'seasons',
-      { number: parseInt(season_id) }
-    );
+      { number: parseInt(req.params.season_id) }
+    )
+  ))
+    success(res, season);
+  else
+    fail(res, null, 'Season not found');
 };
-controller.getEpisode = function(serie_id, season_id, episode_id) {
-  return tree.get('series',
-      { title: serie_id },
+controller.getEpisode = function(req, res) {
+  var episode;
+
+  if ((
+    episode = tree.get(
+      'series',
+      { title: req.params.serie_id },
       'seasons',
-      { number: parseInt(season_id) },
+      { number: parseInt(req.params.season_id) },
       'episodes',
-      { number: parseInt(episode_id) }
-    );
+      { number: parseInt(req.params.episode_id) }
+    )
+  ))
+    success(res, episode);
+  else
+    fail(res, null, 'Episode not found');
 };
-controller.getEpisodeFile = function(serie_id, season_id, episode_id, file_id) {
-  return tree.get('topDirectory') +
-    '/' +
-    tree.get('series',
-      { title: serie_id },
+controller.downloadEpisode = function(req, res) {
+  var episode;
+
+  if ((
+    episode = tree.get(
+      'series',
+      { title: req.params.serie_id },
       'seasons',
-      { number: parseInt(season_id) },
+      { number: parseInt(req.params.season_id) },
       'episodes',
-      { number: parseInt(episode_id) },
+      { number: parseInt(req.params.episode_id) },
       'files',
-      file_id,
+      req.params.file_id,
       'pathToVideo'
-    );
+    )
+  ))
+    res.download(tree.get('topDirectory') + '/' + episode);
+  else
+    fail(res, null, 'Episode not found');
 };
-controller.getMovies = function() {
-  return tree.get('movies');
+controller.streamEpisode = function (req, res) {
+  var episode;
+
+  if ((
+    episode = tree.get(
+      'series',
+      { title: req.params.serie_id },
+      'seasons',
+      { number: parseInt(req.params.season_id) },
+      'episodes',
+      { number: parseInt(req.params.episode_id) },
+      'files',
+      req.params.file_id,
+      'pathToVideo'
+    )
+  )) {
+    res.setHeader('content-type', 'video/mp4');
+    fs.createReadStream(tree.get('topDirectory') + '/' + episode )
+      .pipe(res);
+  }
+  else
+    fail(res, null, 'Episode not found');
 };
-controller.getMovie = function(movie_id) {
-  return tree.get('movies', { title: movie_id });
+
+// Movies
+controller.getMovies = function(req, res) {
+  success(res, tree.get('movies'));
+};
+controller.getMovie = function(req, res) {
+  var movie;
+
+  if ((movie = tree.get('movies', { title: req.params.movie_id })))
+    success(res, movie);
+  else
+    fail(res, null, 'Movie not found');
 };
 
 module.exports = controller;
--- controllers/routes.js
+++ controllers/routes.js
@@ -3,62 +3,23 @@ var express = require('express'),
     fs = require('fs');
 
 module.exports = function(server) {
-  server.get('/disk', function (req, res) {
-    controller.getDisk(function(disk) {
-      res.send(disk);
-    });
-  });
+  server.get('/disk', controller.getDisk);
 
-  server.get('/series', function (req, res) {
-    res.send(controller.getSeries());
-  });
-  server.get('/series/:serie_id', function (req, res) {
-    res.send(controller.getSerie(req.params.serie_id));
-  });
-  server.get('/series/:serie_id/:season_id', function (req, res) {
-    res.send(controller.getSeason(req.params.serie_id, req.params.season_id));
-  });
-  server.get('/series/:serie_id/:season_id/:episode_id', function (req, res) {
-    res.send(
-      controller.getEpisode(
-        req.params.serie_id,
-        req.params.season_id,
-        req.params.episode_id
-      )
-    );
-  });
+  // Series
+  server.get('/series', controller.getSeries);
+  server.get('/series/:serie_id', controller.getSerie);
+  server.get('/series/:serie_id/:season_id', controller.getSeason);
+  server.get('/series/:serie_id/:season_id/:episode_id', controller.getEpisode);
   server.get(
     '/series/:serie_id/:season_id/:episode_id/:file_id',
-    function (req, res) {
-      res.download(
-        controller.getEpisodeFile(
-          req.params.serie_id,
-          req.params.season_id,
-          req.params.episode_id,
-          req.params.file_id
-        )
-      );
-    }
+    controller.downloadEpisode
   );
   server.get(
     '/series/:serie_id/:season_id/:episode_id/:file_id/stream',
-    function (req, res) {
-      res.setHeader('content-type', 'video/mp4');
-      fs.createReadStream(
-        controller.getEpisodeFile(
-          req.params.serie_id,
-          req.params.season_id,
-          req.params.episode_id,
-          req.params.file_id
-        )
-      ).pipe(res);
-    }
+    controller.streamEpisode
   );
 
-  server.get('/movies', function (req, res) {
-    res.send(controller.getMovies());
-  });
-  server.get('/movies/:movie_id', function (req, res) {
-    res.send(controller.getMovie(req.params.movie_id));
-  });
+  // Movies
+  server.get('/movies', controller.getMovies);
+  server.get('/movies/:movie_id',  controller.getMovie);
 };
